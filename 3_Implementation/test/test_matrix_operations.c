bdcdc2dec6b9294ef74492e6088c4cf9a8d6f5ab
Hemanth A - 256889 <57669802+hemanthasapu@users.noreply.github.com>
2021-04-15 13:54:52 +0530

    unit testing

--- 3_Implementation/test/test_matrix_operations.c
+++ 3_Implementation/test/test_matrix_operations.c
@@ -1,16 +1,45 @@
 #include "unity.h"
-#include <matrix_operations.h>
+#include "matrix_operations.h"
 
-/* Modify these two lines according to the project */
-#include <matrix_operations.h>
-#define PROJECT_NAME    "Calculator"
+#define PROJECT_NAME    "Matrix Calculator"
 
 /* Prototypes for all the test functions */
-void test_add(void);
-void test_add_testcase2(void);
-void test_subtract(void);
-void test_multiply(void);
-void test_divide(void);
+
+/**
+ * @brief Testing function for addition of 2 matrices
+ * 
+ */
+void test_add_mat(void);
+
+/**
+ * @brief Testing function for subtraction of 2 matrices
+ * 
+ */
+void test_sub_mat(void);
+
+/**
+ * @brief Testing function for multiplication of 2 matrices
+ * 
+ */
+void test_mul_mat(void);
+
+/**
+ * @brief Testing function for finding determiant of matrix 
+ * 
+ */
+void test_det_mat(void);
+
+/**
+ * @brief Testing function for finding inverse of a function
+ * 
+ */
+void test_inv_mat(void);
+
+/**
+ * @brief To test the power function of (-1)^exp
+ * 
+ */
+void test_pow(void);
 
 /* Required by the unity test framework */
 void setUp(){}
@@ -24,21 +53,153 @@ int main()
   UNITY_BEGIN();
 
 /* Run Test functions */
-  RUN_TEST(test_add);
-  RUN_TEST(test_add_testcase2);
-  RUN_TEST(test_subtract);
-  RUN_TEST(test_multiply);
-  RUN_TEST(test_divide);
+  RUN_TEST(test_add_mat);
+  RUN_TEST(test_sub_mat);
+  RUN_TEST(test_mul_mat);
+  RUN_TEST(test_pow);
+  RUN_TEST(test_det_mat);
+  RUN_TEST(test_inv_mat);
+
 
   /* Close the Unity Test Framework */
   return UNITY_END();
 }
 
 /* Write all the test functions */ 
-void test_add(void) {
-  TEST_ASSERT_EQUAL(30, add(10, 20));
-  TEST_ASSERT_EQUAL(-10, add(10, -20));
-  TEST_ASSERT_EQUAL(-30, add(-10, -20));
-  TEST_ASSERT_EQUAL(10, add(-10, 20));
+void test_add_mat(void) {
+
+  float **test_mat1, **test_mat2;
+  test_mat1 = (float **)calloc(2,sizeof(float));
+  test_mat2 = (float **)calloc(2,sizeof(float));
+    for(int i=0;i<2;i++){
+        test_mat1[i] = (float *)calloc(2,sizeof(float));
+        test_mat2[i] = (float *)calloc(2,sizeof(float));
+    }
+
+  test_mat1[0][0] = 1;
+  test_mat1[0][1] = 2;
+  test_mat1[1][0] = 3;
+  test_mat1[1][1] = 4;
+
+  test_mat2[0][0] = 1;
+  test_mat2[0][1] = 2;
+  test_mat2[1][0] = 3;
+  test_mat2[1][1] = 4;
+
+  TEST_ASSERT_EQUAL(SUCCESS,add_matrices(test_mat1,test_mat2,2));
+
+
+  free(test_mat1);
+  free(test_mat2);
+
 }
 
+void test_sub_mat(void) {
+
+  float **test_mat1, **test_mat2;
+  test_mat1 = (float **)calloc(2,sizeof(float));
+  test_mat2 = (float **)calloc(2,sizeof(float));
+    for(int i=0;i<2;i++){
+        test_mat1[i] = (float *)calloc(2,sizeof(float));
+        test_mat2[i] = (float *)calloc(2,sizeof(float));
+    }
+
+  test_mat1[0][0] = 2;
+  test_mat1[0][1] = 3;
+  test_mat1[1][0] = 4;
+  test_mat1[1][1] = 6;
+
+  test_mat2[0][0] = 1;
+  test_mat2[0][1] = 2;
+  test_mat2[1][0] = 3;
+  test_mat2[1][1] = 4;
+
+  TEST_ASSERT_EQUAL(SUCCESS,subtract_matrices(test_mat1,test_mat2,2));
+
+
+  free(test_mat1);
+  free(test_mat2);
+
+}
+
+void test_mul_mat(void){
+    float **test_mat1, **test_mat2;
+  test_mat1 = (float **)calloc(2,sizeof(float));
+  test_mat2 = (float **)calloc(2,sizeof(float));
+    for(int i=0;i<2;i++){
+        test_mat1[i] = (float *)calloc(2,sizeof(float));
+        test_mat2[i] = (float *)calloc(2,sizeof(float));
+    }
+
+  test_mat1[0][0] = 1;
+  test_mat1[0][1] = 2;
+  test_mat1[1][0] = 3;
+  test_mat1[1][1] = 4;
+
+  test_mat2[0][0] = 1;
+  test_mat2[0][1] = 2;
+  test_mat2[1][0] = 3;
+  test_mat2[1][1] = 4;
+
+  TEST_ASSERT_EQUAL(SUCCESS,product_matrices(test_mat1,test_mat2,2));
+
+
+  free(test_mat1);
+  free(test_mat2);
+
+}
+
+
+void test_det_mat(void){
+      
+    float **test_mat1;
+    test_mat1 = (float **)calloc(2,sizeof(float));
+    for(int i=0;i<2;i++){
+        test_mat1[i] = (float *)calloc(2,sizeof(float));
+    }
+
+  test_mat1[0][0] = 5;
+  test_mat1[0][1] = 3;
+  test_mat1[1][0] = 5;
+  test_mat1[1][1] = 4;
+
+
+  TEST_ASSERT_EQUAL(5.0000,determinant(test_mat1,2));
+
+
+  free(test_mat1);
+
+
+}
+
+void test_inv_mat(void){
+  float **test_mat1;
+    test_mat1 = (float **)calloc(3,sizeof(float));
+    for(int i=0;i<3;i++){
+        test_mat1[i] = (float *)calloc(3,sizeof(float));
+    }
+
+  test_mat1[0][0] = 3;
+  test_mat1[0][1] = 5;
+  test_mat1[0][2] = 2;
+  test_mat1[1][0] = 1;
+  test_mat1[1][1] = 5;
+  test_mat1[1][2] = 8;
+  test_mat1[2][0] = 3;
+  test_mat1[2][1] = 9;
+  test_mat1[2][2] = 2;
+
+
+  TEST_ASSERT_EQUAL(SUCCESS,inverse(test_mat1,3));
+
+  free(test_mat1);
+
+}
+
+void test_pow(void){
+
+
+  TEST_ASSERT_EQUAL(1,power(2));
+  TEST_ASSERT_EQUAL(-1,power(3));
+
+}
